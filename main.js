bff82aab0ebeb8a3946b530f74502df062c83e90
zmc <1246610062@qq.com>
2020-06-18 18:36:30 +0800

    update main 1 2

--- main.js
+++ main.js
@@ -5,3 +5,25 @@ function login(){
 function reg(){
     window.location.href="reg.html";
 }
+
+$(function() {
+    var $slider = $('#demo-slider-0');
+    var counter = 0;
+    var getSlide = function() {
+      counter++;
+      return '<li><img src="/img1/' +
+        (Math.floor(Math.random() * 4) + 1) + '.png" />' +
+        '<div class="am-slider-desc">动态插入的 slide ' + counter + '</div></li>';
+    };
+
+    $('.js-demo-slider-btn').on('click', function() {
+      var action = this.getAttribute('data-action');
+      if (action === 'add') {
+        $slider.flexslider('addSlide', getSlide());
+      } else {
+        var count = $slider.flexslider('count');
+        count > 1 && $slider.flexslider('removeSlide', $slider.flexslider('count') - 1);
+      }
+    });
+
+  });
